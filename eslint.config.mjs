a0bb16ff4b3e968ae4e8ae5947ecfdac840563db
dyparkkk <dyparkkk@lguplus.co.kr>
2025-02-02 20:17:01 +0900

    second commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends('next/core-web-vitals', 'next/typescript'),
+    {
+        rules: {
+            'no-unused-vars': 'off', // 사용하지 않는 변수 경고 비활성화
+            '@typescript-eslint/no-unused-vars': 'off', // TypeScript에서도 사용하지 않는 변수 경고 비활성화
+            "@typescript-eslint/no-explicit-any": "off" // any 타입 사용 허용
+        },
+    },
 ];
 
 export default eslintConfig;
